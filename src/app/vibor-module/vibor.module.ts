c247198a7b57570ee91748521b706ac0bacbd64a
faten <faten@DESKTOP-F66LGDB>
2017-03-09 18:40:06 +0400

    push version

--- src/app/vibor-module/vibor.module.ts
+++ src/app/vibor-module/vibor.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ViborComponent } from './vibor/vibor.component';
@@ -8,4 +8,11 @@ import { ViborComponent } from './vibor/vibor.component';
   declarations: [ ViborComponent ],
   exports: [ ViborComponent ]
 })
-export class ViborModule { }
+export class ViborModule {
+  public static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: ViborModule,
+            providers: [ ]
+        };
+    }
+}
