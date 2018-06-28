3e58ca904a6c61bc75464d010c6286df765de77f
Aleksey Nikitin <aleksey@noblecode.ru>
2018-06-28 15:49:18 +0400

    Работающий код без стилей

--- projects/ng-vibor/src/lib/ng-vibor.module.ts
+++ projects/ng-vibor/src/lib/ng-vibor.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { NgViborComponent } from './ng-vibor.component';
 import { ViborBothDirective, ViborCreateDirective, ViborDropdownDirective, ViborSelectedDirective } from './ng-vibor-template.directive';
 
 @NgModule({
-  imports: [],
+  imports: [
+    FormsModule, BrowserModule
+  ],
   declarations: [
     NgViborComponent,
     ViborBothDirective, ViborCreateDirective, ViborDropdownDirective, ViborSelectedDirective
   ],
-  exports: [NgViborComponent]
+  exports: [
+    NgViborComponent
+  ]
 })
 export class NgViborModule { }
